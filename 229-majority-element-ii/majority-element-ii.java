d53dd229267694fefb553a4c89fbf1335cc557c1
manish9760 <manish97607@gmail.com>
2025-02-16 10:54:21 +0530

    Time: 11 ms (43.31%) | Memory: 47.7 MB (72.47%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -3,12 +3,16 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
         List<Integer> result = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){//true
-                map.put(nums[i],map.get(nums[i])+1);
-            }else {
-                map.put(nums[i],1);
-            }
+        // for(int i=0;i<n;i++){
+        //     if(map.containsKey(nums[i])){//true
+        //         map.put(nums[i],map.get(nums[i])+1);
+        //     }else {
+        //         map.put(nums[i],1);
+        //     }
+        // }
+        // Count occurrences of each element
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         for(int key:map.keySet()){
             if(map.get(key)>n/3){
