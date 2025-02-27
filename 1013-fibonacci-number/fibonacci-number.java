c94bd7abbe6ad60a3471ddf22b3d624233605206
manish9760 <manish97607@gmail.com>
2025-02-27 11:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (78.96%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,9 @@
 class Solution {
-    public static int fibbo(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        else return fibbo(n-1) + fibbo(n-2);
-    }
+    // public static int fibbo(int n){
+    //     if(n==0) return 0;
+    //     if(n==1) return 1;
+    //     else return fibbo(n-1) + fibbo(n-2);
+    // }
     public int fib(int n) {
         // int n1=0,next_term;
         // int n2=1;
@@ -19,7 +19,21 @@ class Solution {
         // return next_term;
 
 
-        int last = fibbo(n);
-        return last;
+        // int last = fibbo(n);
+        // return last;
+
+        if(n==0||n==1) return n;
+        int arr[] = new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<n+1;i++){
+            arr[i]=-1;
+        }
+        return helper(arr,n);
+        
+    }
+    int helper(int arr[],int n){
+        if(arr[n]!=-1) return arr[n];
+        else return arr[n] = helper(arr,n-1)+helper(arr,n-2);
     }
 }
